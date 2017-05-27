f57d674cb4c80d50cea37e9fe797b21f6958d849
Luke1982 <guido@Tims-MacBook-Pro.local>
2017-05-27 14:06:58 +0200

    More robust dependancy loading

--- modules/Schedular/Schedular.js
+++ modules/Schedular/Schedular.js
@@ -6,3 +6,104 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
+window.addEventListener("load", function(){
+	insertStylesheet("modules/Schedular/lib/css/fullcalendar.min.css");
+	insertStylesheet("modules/Schedular/lib/css/scheduler.min.css");
+
+	var scripts = [
+		"modules/Schedular/lib/js/jquery.min.js",
+		"modules/Schedular/lib/js/moment.min.js",
+		"modules/Schedular/lib/js/fullcalendar.min.js",
+		"modules/Schedular/lib/js/scheduler.min.js"
+	];
+
+	function require(scripts, i) {
+		var i = i == undefined ? 0 : i;
+		var script = document.createElement("script");
+		script.src = scripts[i];
+		if ( (i + 1) < scripts.length)
+			script.addEventListener("load", function(){
+				require(scripts, i+1);
+			});
+		else {
+			launchSchedular();
+		}
+		document.head.appendChild(script);
+	}
+
+	require(scripts);
+
+	function insertStylesheet(href) {
+		var link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.href = href;
+		document.head.appendChild(link);
+	}
+
+	function launchSchedular() {
+		var int = setInterval(function(){
+			if (typeof jQuery == "function" && typeof jQuery().fullCalendar == "function" && typeof moment == "function") {
+				$('#schedular').fullCalendar({
+					defaultView: 'agendaDay',
+					defaultDate: '2017-05-07',
+					editable: true,
+					selectable: true,
+					eventLimit: true, // allow "more" link when too many events
+					header: {
+						left: 'prev,next today',
+						center: 'title',
+						right: 'agendaDay,agendaTwoDay,agendaWeek,month'
+					},
+					views: {
+						agendaTwoDay: {
+							type: 'agenda',
+							duration: { days: 2 },
+
+							// views that are more than a day will NOT do this behavior by default
+							// so, we need to explicitly enable it
+							groupByResource: true
+
+							//// uncomment this line to group by day FIRST with resources underneath
+							//groupByDateAndResource: true
+						}
+					},
+
+					//// uncomment this line to hide the all-day slot
+					//allDaySlot: false,
+
+					resources: [
+						{ id: 'a', title: 'Room A' },
+						{ id: 'b', title: 'Room B', eventColor: 'green' },
+						{ id: 'c', title: 'Room C', eventColor: 'orange' },
+						{ id: 'd', title: 'Room D', eventColor: 'red' }
+					],
+					events: [
+						{ id: '1', resourceId: 'a', start: '2017-05-06', end: '2017-05-08', title: 'event 1', backgroundColor : 'black'},
+						{ id: '2', resourceId: 'a', start: '2017-05-07T09:00:00', end: '2017-05-07T14:00:00', title: 'event 2' },
+						{ id: '3', resourceId: 'b', start: '2017-05-07T12:00:00', end: '2017-05-08T06:00:00', title: 'event 3' },
+						{ id: '4', resourceId: 'c', start: '2017-05-07T07:30:00', end: '2017-05-07T09:30:00', title: 'event 4' },
+						{ id: '5', resourceId: 'd', start: '2017-05-07T10:00:00', end: '2017-05-07T15:00:00', title: 'event 5' }
+					],
+
+					select: function(start, end, jsEvent, view, resource) {
+						console.log(
+							'select',
+							start.format(),
+							end.format(),
+							resource ? resource.id : '(no resource)'
+						);
+					},
+					dayClick: function(date, jsEvent, view, resource) {
+						console.log(
+							'dayClick',
+							date.format(),
+							resource ? resource.id : '(no resource)'
+						);
+					}
+				});
+				clearInterval(int);
+			}
+		}, 400);
+	}
+
+});
