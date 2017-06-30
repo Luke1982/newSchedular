904bef65d1325e1e16c7af6b09f6a5cab45e94e5
Luke1982 <gcx3@hotmail.com>
2017-06-30 12:02:25 +0200

    Add english translations to dutch file for testing

--- modules/Schedular/language/nl_nl.lang.php
+++ modules/Schedular/language/nl_nl.lang.php
@@ -9,15 +9,65 @@
  ************************************************************************************/
 
 $mod_strings = Array(
-	'ModuleName' => 'Module Name',
-	'SINGLE_ModuleName' => 'Module Name',
-	'ModuleName ID' => 'Module Name ID',
+	'ModuleName' 					=> 'Schedular',
+	'SINGLE_ModuleName' 			=> 'Module Name',
+	'ModuleName ID' 				=> 'Module Name ID',
 
-	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
-	'LBL_MODULEBLOCK_INFORMATION' => 'ModuleBlock Information',
-	'LBL_DESCRIPTION_INFORMATION' => 'Verkoopkans omschrijving',
+	'LBL_CUSTOM_INFORMATION' 		=> 'Custom Information',
+	'LBL_MODULEBLOCK_INFORMATION' 	=> 'ModuleBlock Information',
+	'LBL_DESCRIPTION_INFORMATION'	=> 'Description',
 
-	'ModuleFieldLabel' => 'ModuleFieldLabel Text',
+	'ModuleFieldLabel' 				=> 'ModuleFieldLabel Text',
+
+	// General strings
+	'resources' 					=> 'Resources',
+	'resource'						=> 'Resource',
+	'relations' 					=> 'Relations',
+	'startdate' 					=> 'Start Date',
+	'enddate'						=> 'End date',
+	'starttime'						=> 'Start Time',
+	'endtime'						=> 'End Time',
+	'title'							=> 'Title',
+	'description'					=> 'Description',
+	'save'							=> 'Save',
+	'cancel'						=> 'Cancel',
+
+	// Field labels
+	'schedular_eventtype'			=> 'Event Type',
+
+	// Settings
+	'av_users' 						=> 'Available users',
+	'save_users' 					=> 'Save users',
+	'background_color' 				=> 'Background color',
+	'save_eventsettings' 			=> 'Save event settings',
+	'select_module' 				=> 'Select module',
+	'add_relation' 					=>  'Add Relation',
+	'filter_fields' 				=> 'Search in fields',
+	'return_fields' 				=> 'Return fields',
+	'include_relation_id' 			=> 'Include relation ID',
+	'relation_filterfield' 			=> 'Filter on field from included relation',
+	'business_hours_start' 			=> 'Start of business hours',
+	'business_hours_end' 			=> 'End of business hours',
+	'save_general_settings' 		=> 'Save general settings',
+	'general_settings'				=> 'General settings',
+
+	// Settings toasts
+	'toast_users_saved' 			=> 'Available users saved to database',
+	'toast_eventtypes_saved' 		=> 'Event type preferences saved',
+	'toast_relation_updated' 		=> 'Relation updated',
+	'toast_relation_exists' 		=> 'Relation already exists',
+	'toast_relation_removed' 		=> 'Relation removed',
+	'toast_general_settings_saved'	=> 'General settings saved',
+
+	// Event types
+	'Checkup' 						=> 'a Checkup',
+	'Visit' 						=> 'a Visit',
+	'Service'						=> 'perform Service',
+
+	// Event UI
+	'clicktodrag' 					=> 'Click and drag here',
+	'searchin'						=> 'Search in',
+	'currentlylinked'				=> 'Currently linked'
 );
 
 ?>
