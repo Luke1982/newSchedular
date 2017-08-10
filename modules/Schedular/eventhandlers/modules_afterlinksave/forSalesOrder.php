1c2093a06126cecf9f7358d5934fc4d1df6c5880
Luke1982 <gcx3@hotmail.com>
2017-08-10 12:33:41 +0200

    Change the assigned user ID when a salesorder is linked to a schedular item

--- modules/Schedular/eventhandlers/modules_afterlinksave/forSalesOrder.php
+++ modules/Schedular/eventhandlers/modules_afterlinksave/forSalesOrder.php
@@ -4,3 +4,4 @@
 // $linked_focus is the related module's record
 $linked_focus->column_fields['sostatus'] = 'Ingepland';
 $linked_focus->column_fields['duedate'] = $source_focus->column_fields['schedular_startdate'];
+$linked_focus->column_fields['reports_to_id'] = $source_focus->column_fields['assigned_user_id'];
