cd2290ef7c57ccaef06cbc39cf2863455fea6b10
Luke1982 <gcx3@hotmail.com>
2018-07-17 15:10:36 +0200

    Set a request variable that indicates deletion of the schedular record for use in further workflows

--- modules/Schedular/eventhandlers/modules_beforerecorddelete/forSalesOrder.php
+++ modules/Schedular/eventhandlers/modules_beforerecorddelete/forSalesOrder.php
@@ -1,5 +1,6 @@
 <?php
 
 // $red is the record focus
+$_REQUEST['schedular_deletion'] = true;
 $rec->column_fields['sostatus'] = 'Created';
-$rec->column_fields['duedate'] = "";
+$rec->column_fields['duedate'] = '';
