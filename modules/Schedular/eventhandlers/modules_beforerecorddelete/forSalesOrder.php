90e7ee0836f50284e2eefb3359743a998960365b
Luke1982 <gcx3@hotmail.com>
2022-03-17 15:59:55 +0100

    Revert to earliest possible appointment when record is deleted but is not the last one related to that SO

--- modules/Schedular/eventhandlers/modules_beforerecorddelete/forSalesOrder.php
+++ modules/Schedular/eventhandlers/modules_beforerecorddelete/forSalesOrder.php
@@ -18,5 +18,24 @@ $_REQUEST['remaining_schedular_count'] = $sch_count;
 
 if ((int) $sch_count === 0) {
 	$rec->column_fields['sostatus'] = 'Created';
+	$rec->column_fields['duedate'] = '';
+	$rec->column_fields['reports_to_id'] = '';
+} else {
+	$r = $adb->query(
+		"SELECT sch.schedular_startdate AS newduedate,
+				ce.smownerid AS newuser
+		FROM vtiger_crmentityrel 
+			INNER JOIN vtiger_crmentity ce ON vtiger_crmentityrel.crmid = ce.crmid
+			INNER JOIN vtiger_schedular sch ON ce.crmid = sch.schedularid
+		WHERE vtiger_crmentityrel.relcrmid = {$rec->id}
+		AND vtiger_crmentityrel.crmid != {$sch_id}
+		AND module = 'Schedular'
+		AND ce.deleted = 0
+		AND sch.schedular_provisional != 1
+		AND sch.schedular_eventstatus != 'Cancelled'
+		ORDER BY sch.schedular_startdate ASC LIMIT 1"
+	);
+	$first_remaining_appointment = $adb->fetch_array($r);
+	$rec->column_fields['duedate'] = $first_remaining_appointment['newduedate'];
+	$rec->column_fields['reports_to_id'] = $first_remaining_appointment['newuser'];
 }
-$rec->column_fields['duedate'] = '';
